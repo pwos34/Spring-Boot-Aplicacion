4b4bc1c67805cd9c5262000c9a20b800dc75f4bd
Pedro Alberto Hernandez Gomez <pahg34@gmail.com>
2020-04-22 02:00:27 -0500

    User and Role created. Close #2

---
+++ src/main/java/com/chinche/Aplicacion/entity/Role.java
@@ -0,0 +1,99 @@
+package com.chinche.Aplicacion.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Role implements Serializable {
+
+
+	private static final long serialVersionUID = -6972936908444712376L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO,generator = "native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long Id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [Id=" + Id + ", name=" + name + ", description=" + description + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
